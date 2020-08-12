1fe5d35a5c4dd644bac56e07046ab8cf76f530d3
maituongluanvn <maituongluanvn@gmail.com>
2020-08-12 23:38:41 +0700

    add eslint & prettier

--- app/midlerwares/verifyToken.js
+++ app/midlerwares/verifyToken.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
-	const token = req.header('auth-token')
-	if (!token) return res.status(401).send('Need Access')
-	try {
-		const verify = jwt.verify(token, process.env.TOKEN_SECRET)
-        req.user = verify
-        next()
-	} catch (error) {
-		res.status(400).send(error)
-	}
+  const token = req.header('auth-token')
+  if (!token) return res.status(401).send('Need Access')
+  try {
+    const verify = jwt.verify(token, process.env.TOKEN_SECRET)
+    req.user = verify
+    next()
+  } catch (error) {
+    res.status(400).send( error )
+  }
 }
--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose')
 const autoIncrement = require('mongoose-auto-increment')
 var usersSchema = new mongoose.Schema(
-	{
-		// _id: { type: Number, autoIndex: true },
-		firstName: { type: String, required: true },
-		lastName: { type: String, required: true },
-		password: { type: String, required: true },
-		email: { type: String, required: true },
-		// userID: { type: mongoose.Types.ObjectId, ref: 'users' },
-		role: { type: Number, required: true, default: 20, ref: 'roles' },
-	},
-	{ timestamps: true }
+  {
+    // _id: { type: Number, autoIndex: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    // userID: { type: mongoose.Types.ObjectId, ref: 'users' },
+    role: { type: Number, required: true, default: 20, ref: 'roles' },
+  },
+  { timestamps: true }
 )
 usersSchema.plugin(autoIncrement.plugin, 'users')
 
