a2487ba8d12d3c0f78e2ae39c05817f3611c2250
maituongluanvn <maituongluanvn@gmail.com>
2020-09-03 22:17:22 +0700

    commit

--- config/db.js
+++ config/db.js
@@ -1,21 +1,14 @@
-const mongoose = require('mongoose')
-const autoIncrement = require('mongoose-auto-increment')
+const { Pool } = require('pg')
 
-const connectDB = async () => {
-  try {
-    autoIncrement.initialize(mongoose)
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
+const pool = new Pool({
+  user: 'pjfneujs',
+  host: 'topsy.db.elephantsql.com',
+  database: 'pjfneujs',
+  password: 'yxcNyYCIyo0I53zkb5B1FJGbKrCcdpvE',
+  port: 5432,
+  max: 100,
+})
 
-    console.log(`MongoDB connected: ${conn.connection.host}`)
-  } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }
-}
-
-module.exports = connectDB
+pool.on('error', (err, client) => {
+  console.error('Database Error :', err)
+})
